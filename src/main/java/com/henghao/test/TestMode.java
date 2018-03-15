791d5e582df550bd9b5814016e62d9989b334115
zcy <824318340@qq.com>
2018-03-15 17:23:21 +0800

    add hello git

--- src/main/java/com/henghao/test/TestMode.java
+++ src/main/java/com/henghao/test/TestMode.java
@@ -1,13 +1,17 @@
 package com.henghao.test;
 
 
+import javafx.print.Printer;
+
 /**
  * Created by admin on 2018/1/18.
  */
 public class TestMode {
 
     public static void main(String[] args) {
+
         System.out.println("hello idea!");
+        System.out.println("hello idea! hello git");
     }
 
 }
